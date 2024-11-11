a51360b8ded9ebe06e4d042e49b9fc9fad655b00
cshyeon32 <139736071+cshyeon32@users.noreply.github.com>
2024-11-11 16:20:25 +0900

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import Spline from '@splinetool/react-spline';
+import { useNavigate } from 'react-router-dom';
 const Home = () => {
+const navigate = useNavigate();
     return (
-        <React.Fragment>
-            asdf
-        </React.Fragment>
+        <>
+            <div className='Text' style={{position: 'absolute', width: '50%', top: '30%'}}>
+                <span>"새로운 사람과의 랜덤 채팅"</span>
+                <span>"45초 짧은 시간 채팅"</span>
+                <button onClick={()=>navigate('chat')}>"채팅 시작"</button>
+            </div>
+            <Spline scene="https://prod.spline.design/klTPcT8rlztcPKJh/scene.splinecode" />
+        </>
     );
 };
 
--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home/Home";
-
+import Home from "../pages/home/Home";
+import Chat from "../pages/chat/Chat";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="chat" element={<Chat/>}/>
       </Routes>
     </Router>
   );
